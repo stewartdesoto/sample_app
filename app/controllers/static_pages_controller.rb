92c4ad84041b876ed3ca9d97e787c53974d9f8ad
Stewart DeSoto <stewartdesoto@yahoo.com>
2015-08-09 13:11:37 -0500

    add full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-  	render 'home'
   end
 
   def help
-  	render 'help'
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+#returns the full title on a per-page basis
+	def full_title(page_title = '')
+		title = "Rails Tutorial"
+		title = page_title + " | " + title unless page_title.empty?
+		title
+	end
 end
